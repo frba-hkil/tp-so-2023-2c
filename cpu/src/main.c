fcc1cc5308c060ba5206ca8297a4147c81b9a3f8
JuanValcarce0 <vjuan@frba.utn.edu.ar>
2023-09-19 04:40:30 +0000

    ejecucion de instrucciones basicas

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -9,19 +9,29 @@ int main(int argc, char *argv[]) {
     }
 
     // TEST 
+
+    //t_pcb *testPcb = crear_pcb(1, 1, list_create(), 1, 1);
+
     // char *code = string_new();
     // string_append(&code, "SET AX 11\n");
-    // string_append(&code, "SET BX 23\n");
-    // string_append(&code, "ADD AX BX\n");
+    // string_append(&code, "SET BX 50\n");
+    // string_append(&code, "SUM AX BX\n");
+    // string_append(&code, "SET CX 5\n");
+    // string_append(&code, "SUB BX CX\n");
     // string_append(&code, "EXIT");
 
-    // procesarInstrucciones(code);
+    // procesarInstrucciones(code, testPcb);    
+
+    // printf("AX %d\n", testPcb->AX);
+    // printf("BX %d\n", testPcb->BX);
+    // printf("CX %d\n", testPcb->CX);
+    // printf("DX %d\n", testPcb->DX);
 
     config_destroy(cfg);
     return 0;
 }
 
-void procesarInstrucciones(char *codigo) {
+void procesarInstrucciones(char *codigo, t_pcb *pcb) {
     char **rows = string_split(codigo, "\n"), **cells;
     int i = 0;
 
@@ -39,13 +49,8 @@ void procesarInstrucciones(char *codigo) {
                 k++;
             }
         }
-        
-        // TEST 
-        // puts(instruccion);
-        // if (args != NULL) {
-        //     puts(args[0]);
-        //     puts(args[1]);
-        // }
+
+        ejecutarInstruccion(instruccion, args, pcb);
 
         i++;
     } 
@@ -74,5 +79,25 @@ uint32_t get_registro(t_pcb *pcb, char *reg) {
         return pcb->DX;
 
     return 0;
+}
+
+uint32_t transform_value(t_pcb *pcb, char *val) {
+    for (int i = 0; i < 4; i++) {
+        if (strcmp(nombres_registros[i], val) == 0) {
+            return get_registro(pcb, val);
+        }
+    }
+
+    return atoi(val);
+}
+
+void ejecutarInstruccion(char *tipo, char **args, t_pcb *pcb) {
+    if (strcmp(tipo, "SET") == 0) {
+        set_registro(pcb, args[0], transform_value(pcb, args[1]));
+    } else if (strcmp(tipo, "SUM") == 0) {
+        set_registro(pcb, args[0], get_registro(pcb, args[0]) + get_registro(pcb, args[1]));
+    } else if (strcmp(tipo, "SUB") == 0) {
+        set_registro(pcb, args[0], get_registro(pcb, args[0]) - get_registro(pcb, args[1]));
+    }
 }
 
--- cpu/src/main.h
+++ cpu/src/main.h
@@ -7,6 +7,10 @@
 #include "../../utils/src/utils.h"
 #include "../../utils/src/estructuras.h"
 
+char *nombres_registros[4] = {"AX", "BX", "CX", "DX"};
 
-void processInstruction(char *code);uint32_t get_registro(t_pcb *pcb, char *reg);
+void procesarInstrucciones(char *code, t_pcb *pcb);
+void ejecutarInstruccion(char *tipo, char **args, t_pcb *pcb);
+void set_registro(t_pcb *pcb, char *reg, u_int32_t val);
+uint32_t get_registro(t_pcb *pcb, char *reg);
 uint32_t transform_value(t_pcb *pcb, char *val);
