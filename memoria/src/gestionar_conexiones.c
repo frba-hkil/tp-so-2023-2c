20b761c727b8a4a433e684b219cb31652b24dfb2
BrianLDel <blucanadelgadillo@frba.utn.edu.ar>
2023-11-19 00:37:23 +0000

    envio instruccion

--- memoria/src/gestionar_conexiones.c
+++ memoria/src/gestionar_conexiones.c
@@ -9,14 +9,17 @@ void procesar_conexiones(t_cliente *datos_cliente) {
 	switch (paquete->codigo_operacion) {
 		// KERNEL
 		case INICIALIZACION_DE_PROCESO:
-			pcb = (paquete);
-			log_info(memoria_logger, "Inicializando estructuras de PID[%d]...", pcb->id);
-			swap_crear_archivo(pcb->id, pcb->tamanio_proceso);
-			uint32_t tabla_primer_nivel = crear_tablas_de_paginacion(pcb->id);
-			enviar_direccion_tabla_primer_nivel(datos_cliente->socket, tabla_primer_nivel);
+			uint32_t tamanio_proceso = recibir_proceso(paquete);
+			//log_info(memoria_logger, "Inicializando estructuras de PID[%d]...", pcb->id);
+			//swap_crear_archivo(pcb->id, pcb->tamanio_proceso);
+			//uint32_t tabla_primer_nivel = crear_tablas_de_paginacion(pcb->id);
+			//enviar_direccion_tabla_primer_nivel(datos_cliente->socket, tabla_primer_nivel);
 
-			eliminar_pcb(pcb);
+			//eliminar_pcb(pcb);
 			break;
+
+		case INSTRUCCION:
+			enviar_instruccion(paquete, datos_cliente->socket);
 		default:
 			log_error(memoria_logger, "Protocolo invalido.");
 			break;
@@ -39,6 +42,23 @@ uint32_t recibir_proceso(t_paquete* paquete){
 	return tamanio_proceso;
 }
 
+void enviar_instruccion(t_paquete* paquete, int socket_fd){
+	t_list* datos = deserealizar_paquete(paquete);
+
+	uint32_t pid = *(uint32_t *)list_get(datos, 0);
+	uint32_t program_counter = *(uint32_t *)list_get(datos, 1);
+
+	t_list* instrucciones = dictionary_get(instrucciones_de_procesos, pid);
+
+	t_instruccion* instruccion = list_get(instrucciones, program_counter);
+
+	t_paquete* paquete_instruccion = serializar_instruccion(instruccion, INSTRUCCION);
+
+	enviar_paquete(paquete_instruccion, socket_fd);
+
+	eliminar_paquete(paquete_instruccion);
+}
+
 void enviar_direccion_tabla_primer_nivel(int socket_fd, uint32_t direccion) {
 	enviar_datos(socket_fd, &direccion, sizeof(uint32_t));
 }
@@ -49,28 +69,28 @@ void informar_estado_proceso(int socket_fd, t_protocolo protocolo) {
 
 void enviar_estructura_traductora(int socket_fd, t_traductor *traductor) {
 	t_paquete *paquete = serializar_traductor(traductor, HANDSHAKE_INICIAL);
-	retardo_respuesta(memoria_config->retardo_memoria);
+	retardo_respuesta(memoria_config->RETARDO_RESPUESTA);
 	enviar_paquete(paquete, socket_fd);
 	eliminar_paquete(paquete);
 }
 
 void enviar_direccion_tabla_segundo_nivel(int socket_fd, uint32_t direccion) {
-	retardo_respuesta(memoria_config->retardo_memoria);
+	retardo_respuesta(memoria_config->RETARDO_RESPUESTA);
 	enviar_datos(socket_fd, &direccion, sizeof(uint32_t));
 }
 
 void enviar_numero_marco_de_pagina(int socket_fd, uint32_t numero) {
-	retardo_respuesta(memoria_config->retardo_memoria);
+	retardo_respuesta(memoria_config->RETARDO_RESPUESTA);
 	enviar_datos(socket_fd, &numero, sizeof(uint32_t));
 }
 
 void enviar_valor_leido_de_memoria(int socket_fd, uint32_t valor) {
-	retardo_respuesta(memoria_config->retardo_memoria);
+	retardo_respuesta(memoria_config->RETARDO_RESPUESTA);
 	enviar_datos(socket_fd, &valor, sizeof(uint32_t));
 }
 
 void informar_escritura_realizada(int socket_fd, t_protocolo protocolo) {
-	retardo_respuesta(memoria_config->retardo_memoria);
+	retardo_respuesta(memoria_config->RETARDO_RESPUESTA);
 	enviar_datos(socket_fd, &protocolo, sizeof(t_protocolo));
 }
 
